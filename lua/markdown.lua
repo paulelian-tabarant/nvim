7d15ca6c6cc5a8652d3fb92f3f901231cc6fc389
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2026-04-19 15:25:01 +0200

    refactor: make markdown settings more idiomatic

--- lua/markdown.lua
+++ lua/markdown.lua
@@ -1,29 +1,36 @@
 -- Markdown configuration
 
 -- Plugin settings
-vim.g.vim_markdown_conceal = 2
-vim.g.vim_markdown_conceal_code_blocks = 0
-vim.g.vim_markdown_math = 1
-vim.g.vim_markdown_toml_frontmatter = 1
-vim.g.vim_markdown_frontmatter = 1
-vim.g.vim_markdown_strikethrough = 1
-vim.g.vim_markdown_autowrite = 1
-vim.g.vim_markdown_edit_url_in = 'tab'
-vim.g.vim_markdown_follow_anchor = 1
+local markdown_settings = {
+  vim_markdown_conceal = 2,
+  vim_markdown_conceal_code_blocks = 0,
+  vim_markdown_math = 1,
+  vim_markdown_toml_frontmatter = 1,
+  vim_markdown_frontmatter = 1,
+  vim_markdown_strikethrough = 1,
+  vim_markdown_autowrite = 1,
+  vim_markdown_edit_url_in = 'tab',
+  vim_markdown_follow_anchor = 1,
+}
+
+for key, value in pairs(markdown_settings) do
+  vim.g[key] = value
+end
 
 -- Autocmds for markdown and text files
-local markdown_group = vim.api.nvim_create_augroup('MarkdownSettings', { clear = true })
-
 vim.api.nvim_create_autocmd('FileType', {
-  group = markdown_group,
+  group = vim.api.nvim_create_augroup('MarkdownSettings', { clear = true }),
   pattern = { 'markdown', 'txt' },
   callback = function()
+    -- Editor settings for prose writing
     vim.opt_local.cursorline = true
     vim.opt_local.spell = true
     vim.opt_local.spelllang = 'fr'
     vim.opt_local.wrap = true
     vim.opt_local.linebreak = true
-    vim.cmd('Goyo 100')
+
+    -- Auto-enable distraction-free mode
+    vim.cmd.Goyo('100')
   end,
   desc = 'Markdown and text file settings'
 })
