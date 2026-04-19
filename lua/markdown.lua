2ae7db7e209f4668bc30b9d1e5b94aeb4e43d89b
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2026-04-19 15:42:38 +0200

    refactor: extract markdown plugin specific configuration in separate file

--- lua/markdown.lua
+++ lua/markdown.lua
@@ -1,21 +1,7 @@
--- Markdown configuration
+-- Markdown writing workflow configuration
 
--- Plugin settings
-local markdown_settings = {
-  vim_markdown_conceal = 2,
-  vim_markdown_conceal_code_blocks = 0,
-  vim_markdown_math = 1,
-  vim_markdown_toml_frontmatter = 1,
-  vim_markdown_frontmatter = 1,
-  vim_markdown_strikethrough = 1,
-  vim_markdown_autowrite = 1,
-  vim_markdown_edit_url_in = 'tab',
-  vim_markdown_follow_anchor = 1,
-}
-
-for key, value in pairs(markdown_settings) do
-  vim.g[key] = value
-end
+-- Load plugin configs
+require('plugins.vim-markdown')
 
 -- Autocmds for markdown and text files
 vim.api.nvim_create_autocmd('FileType', {
